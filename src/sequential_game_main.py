a328c90849cd864a8c1d17a727948fd86564daa4
omnitex <omnitex.git@gmail.com>
2025-11-29 14:05:12 +0100

    Create directory for results if it does not exist

--- src/sequential_game_main.py
+++ src/sequential_game_main.py
@@ -2,7 +2,7 @@ import argparse
 from sequential_game_agent import Agent, Game
 from tqdm import tqdm
 import json
-
+import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -16,6 +16,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     result_save_dir = f'result/single_round/{args.model}/{args.game}_{args.personality}_negotation_{args.max_negotiation_round}.json'
+
+    # Create directory structure if it doesn't exist
+    os.makedirs(os.path.dirname(result_save_dir), exist_ok=True)
 
     args.system_prompt = f'You are a {args.personality} assistant that carefully answer the question.'
     decisions = []
--- src/simultaneous_game_main.py
+++ src/simultaneous_game_main.py
@@ -2,6 +2,7 @@ import argparse
 from simultaneous_game_agent import Agent, Game
 from tqdm import tqdm
 import json
+import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -16,6 +17,9 @@ if __name__ == '__main__':
 
 
     result_save_dir = f'result/single_round/{args.model}/{args.game}_{args.personality}_negotiation_{args.max_negotiation_round}.json'
+
+    # Create directory structure if it doesn't exist
+    os.makedirs(os.path.dirname(result_save_dir), exist_ok=True)
 
     args.system_prompt = f'You are a {args.personality} assistant that carefully answer the question.'
     decisions = []
